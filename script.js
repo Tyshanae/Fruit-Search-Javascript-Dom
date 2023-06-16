adf985bda05c6b0640f3c9231047704dae00f38c
Tyshanae <123408876+Tyshanae@users.noreply.github.com>
2023-06-16 13:57:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+input.addEventListener("keyup", searchHandler);
+
+function searchHandler(e) {
+  let userData = e.target.value;
+  let results = search(userData);
+  showSuggestions(results, userData);
+}
+function useSuggestion(e) {
+  let useSuggestionData = e.textContent;
+  input.value = useSuggestionData;
+  suggestions.innerHTML = "";
+}
+function showSuggestions(results, inputVal) {
+  suggestions.innerHTML = results.map((list) => "<li>" + list + "</li>").join(" ");
+  if (inputVal === "") {
+    suggestions.innerHTML = "";
+  }
+  let allList = suggestions.querySelectorAll("li");
+  for (let i = 0; i < allList.length; i++) {
+    allList[i].setAttribute("onclick", "useSuggestion(this)");
+  }
+}
+
+function search(str) {
+  return fruit.filter((data) => data.toLocaleLowerCase().includes(str.toLocaleLowerCase()));
+}
+
